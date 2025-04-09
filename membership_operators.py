bf0a85a7ef6f85853c5d3acb2900e3450cfe5533
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-09 19:02:55 +0000

    add membership operators

--- membership_operators.py
+++ membership_operators.py
@@ -4,7 +4,6 @@ print ( 'sakil'  in "tareq rahman sakil")
 print ( 'sakil'   not in "tareq rahman sakil")
 
 
-
 player = ['Tamim','shakib','virat','rohit','babar','rezwan','starc','wood','nahid']
 print ('Tamim' in player)
 player = ['Tamim','shakib','virat','rohit','babar','rezwan','starc','wood','nahid']
