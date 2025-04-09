11d19a0892edb288bcc8b9db3e36e7a07f7cd360
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-09 19:30:22 +0000

    add user_operators

---
+++ user_input.py
@@ -0,0 +1,7 @@
+#Type casting (ek data type theke onno data type e newa mane type casting)
+number_1 = int (input('Enter your number: '))
+print (number_1 + 5)
+#another casting
+num_1 = 20
+num_2 = '25'
+print   (str( num_1) + num_2)
