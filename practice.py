ce45dbad4b5bd4c588f68f8a8ef08542e4cd369f
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-10 06:12:47 +0000

    add practice

---
+++ practice.py
@@ -0,0 +1,50 @@
+# variable operators
+
+a = 5
+c = 4
+print(a*c)
+
+# arithmatic operators
+ 
+print (a + c)
+print (a - c)
+print (a / c)
+print (a // c)
+print (a * c)
+print (a ** c)
+print (a % c)
+
+  #comparison operators
+
+b = 50
+d = 40
+if b == d:
+     print ("False")
+elif b >= d:
+    print("True") 
+
+# logical operators
+ 
+
+print ((a>=c) and (b>d)) #and condition
+print ((a<b) or (c>d))   # or condition
+x = False
+print (not x) #not condition
+
+# membershiop operators]
+
+print ('teacher' in "i am a teacher") 
+print ('student' in "i am a teacher") 
+print ('student' not in "i am a teacher") 
+print ('teacher' not in "i am a teacher") 
+
+
+#user input
+# type costing
+Name = int (input('Enter your number:'))
+print(Name + 5)
+
+#another costing
+id_1 = 20
+id_2 = '25'
+print (str(id_1) + id_2)
