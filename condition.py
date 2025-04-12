9a3d0f67072639cd1a3caa6857280dfeb8930896
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-12 19:49:42 +0000

    add condition

--- condition.py
+++ condition.py
@@ -1,16 +1,16 @@
 
-# total_run = 400
-# total_wicket = 20
+# # total_run = 400
+# # total_wicket = 20
 
-# if total_run < int (input("Enter your Run:")) :
+# # if total_run < int (input("Enter your Run:")) :
 
-#     print("Selected For Batsman")
+# #     print("Selected For Batsman")
 
-# elif total_wicket <   int (input("Enter your wicket:")) : 
-#     print("Selected for Allrounder")
+# # elif total_wicket <   int (input("Enter your wicket:")) : 
+# #     print("Selected for Allrounder")
 
-# else:
-#     print("Need more Practice") 
+# # else:
+# #     print("Need more Practice") 
 
 
 
@@ -19,14 +19,32 @@ Number = int (input("Enter the number:"))
 
 
 if Number % 3== 0 and Number % 5 == 0:
-    print("FizzaBuzz")
+     print("FizzaBuzz")
 
 elif Number % 3 == 0:
-    print("Fizz")
+     print("Fizz")
 
 elif Number % 5 == 0:
-    print("Buzz")    
+     print("Buzz")    
 
 
 else:
     print("This not a FizzaBuzz number")    
+
+
+
+
+    ## agree = int (input("Insert you decision: "))
+
+
+agree = int (input("Insert you decision: "))
+
+Free = ('foods','warter','gas','electracity','service','hospital cost')
+cost = ('tranformasion','tution fee','self cost',)
+
+total = cost 
+
+if 0 < agree :
+    print(Free)
+else:
+    print(cost)
