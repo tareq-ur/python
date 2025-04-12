b3d56b2c8e30f0e8a449836b63d1370226b63eaa
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-12 19:33:44 +0000

    add list

--- List.py
+++ List.py
@@ -11,3 +11,14 @@ print(num) #append
 id = [3,4,5,7,8]
 id_1 = id.insert(0,1)
 print(id) #insert
+
+
+Free = ['foods','warter','gas','electracity','service','hospital cost']
+cost = ['tranformasion','tution fee','self cost',]
+
+Free.insert(3, 'darwan')
+cost.append('room rent')
+
+total = cost
+
+print (Free + total)
