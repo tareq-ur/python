0c87ad3c6f910bc61685acf90b50e396ca03206a
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-12 05:09:30 +0000

    add loops

--- loops.py
+++ loops.py
@@ -1,9 +1,15 @@
 # for loops
-id = [ 2,6,7,8,4,54,7,8,151,41,78,67,20]
-for i in id:
-    if 20 in id:
-        print("got it")
-    else:
-        print("pina")
+# id = [ 2,6,7,8,4,54,7,8,151,41,78,67,20]
+# for i in id:
+#     if 20 in id:
+#         print("got it")
+#     else:
+#         print("pina")
 # 20 in id
 # print("yes")
+ # while loops
+count = 0
+while count < 5:
+    print(count)
+    count += 1    #(count = count + 1)
+    
