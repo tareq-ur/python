e866b92d1b7f71088af24a644a57aca62ea3daae
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-13 06:24:17 +0000

    add function

--- funtion.py
+++ funtion.py
@@ -1,3 +1,11 @@
+# two types funtion 
+# Buildin function
+# customs function
+
+
+#Customs Function 
+
+#named funtion
 num_1 = 34
 num_2 = 12
 
@@ -5,3 +13,10 @@ def sum  (num_1 , num_2):
     print (num_1 + num_2)
 
 sum(45,67)    
+
+
+#lambda (anonimous function). when the funtion is small that time we use lambda functiion otherwise use named function
+
+add = lambda x,y : x+y
+
+print(add(23,67))
