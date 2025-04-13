2fa3831ff72107b38681bb6fc60bc27128b75f0b
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-13 05:27:26 +0000

    add toule

--- touple.py
+++ touple.py
@@ -1,7 +1,12 @@
+#we now touple data can not modify that is why we did transfer touple to list then we can modify when we complete our modify again 
+# we did transfer list to touple 
+
 Free = ('foods','warter','gas','electracity','service','hospital cost')
-cost = ('tranformasion','tution fee','self cost',)
+
+Free = list(Free)
+Free.insert (2,'transformation')
+Free = tuple(Free)
+
+print(Free)
 
 
-total = cost
-
-print (Free + total)
