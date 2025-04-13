309a9a6bcca01e39adacf0d8e0454131419b8a25
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-13 19:09:45 +0000

    add loops

--- loops.py
+++ loops.py
@@ -7,9 +7,22 @@
 #         print("pina")
 # 20 in id
 # print("yes")
+
+fruits = ['banana', 'mango', 'apple']
+for index, fruits  in enumerate (fruits):
+    print(index, fruits) # it's provide values and values address
+
+
+
+
+
+
+
+
+
  # while loops
-count = 0
-while count < 5:
-    print(count)
-    count += 1    #(count = count + 1)
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1    #(count = count + 1)
     
