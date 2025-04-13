d1dc88e7e92c543e1bd71b45f9ffb244148838a4
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-13 09:24:09 +0000

    add module

--- function1.py
+++ function1.py
@@ -12,11 +12,20 @@ num_2 = 12
 def sum  (num_1 , num_2):
     print (num_1 + num_2)
 
-sum(45,67)    
+sum(100,50)    
+
+def sub  (num_1 , num_2):
+    print (num_1 - num_2)
+
+sub(100,67) 
+
+
+
+
 
 
 #lambda (anonimous function). when the funtion is small that time we use lambda functiion otherwise use named function
 
-add = lambda x,y : x+y
+# add = lambda x,y : x+y
 
-print(add(23,67))
+# print(add(23,67))
---
+++ module.py
@@ -0,0 +1,6 @@
+# we use modules for shortage program, fast run, maintane time complexsity and space complexsity.
+import function1
+from function1 import sum
+
+print(sum)
+
