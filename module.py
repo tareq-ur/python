dbb2e4f08b8bb2c48ffcf256aed8d8c8f855b8e4
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-13 18:55:31 +0000

    add module

--- module.py
+++ module.py
@@ -1,6 +1,14 @@
 # we use modules for shortage program, fast run, maintane time complexsity and space complexsity.
 import function1
-from function1 import sum
+from function1 import sum as s
 
-print(sum)
+print(s)
 
+#buildin module 
+#cheack for datetime
+from datetime import  datetime as dt
+print(dt.now())
+print(dt.now().hour)
+print(dt.now().minute)
+print(dt.now().second)
+print(dt.now().second)
