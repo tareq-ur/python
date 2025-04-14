60343929903ac9eccb7f10305d9c6c4d6d6afb15
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-14 05:37:08 +0000

    add list

--- List.py
+++ List.py
@@ -1,24 +1,29 @@
-num = [' tareq', 'rahman']
+# num = [' tareq', 'rahman']
 
-num.append('sakil')
+# num.append('sakil')
 
-print(num) #append
-
-
+# print(num) #append
 
 
 
-id = [3,4,5,7,8]
-id_1 = id.insert(0,1)
-print(id) #insert
 
 
-Free = ['foods','warter','gas','electracity','service','hospital cost']
-cost = ['tranformasion','tution fee','self cost',]
+# id = [3,4,5,7,8]
+# id_1 = id.insert(0,1)
+# print(id) #insert
 
-Free.insert(3, 'darwan')
-cost.append('room rent')
 
-total = cost
+# Free = ['foods','warter','gas','electracity','service','hospital cost']
+# cost = ['tranformasion','tution fee','self cost',]
 
-print (Free + total)
+# Free.insert(3, 'darwan')
+# cost.append('room rent')
+
+# total = cost
+
+# print (Free + total)
+
+value = [3,4,5,8,6,4,8,34,5]
+
+value_1 = sum( value)
+print(value_1)
--- string.py
+++ string.py
@@ -9,4 +9,7 @@ print (f'my name is {name}, age is {age}')
 
 
 #option-2
-print('my name is {}, my age is {}'.format(name, age))
+print('my name is {}, my age is {}'.format(name, age)) 
+
+
+# .strip()
