42909e0436e24de4d934e8669b83f7a1b45ea710
Tarequr Rahman <tarequrrahmansakil@gmail.com>
2025-04-16 14:43:06 +0000

    add arithmatic_operators

--- arithmatic_operators.py
+++ arithmatic_operators.py
@@ -1,11 +1,26 @@
-a = 10
-b = 5
+# a = 10
+# b = 5
 
-print( a + b)
-print( a - b)
-print( a * b)
-print( a / b)
-print( a // b) # when we use // it's not provide float number it's provide integer number.
+# print( a + b)
+# print( a - b)
+# print( a * b)
+# print( a / b)
+# print( a // b) # when we use // it's not provide float number it's provide integer number.
 
-print( a % b) # mod, modulus, it's gives us reminder
-print( a ** b) #a^b, power
+# print( a % b) # mod, modulus, it's gives us reminder (vab sesh)
+# print( a ** b) #a^b, power
+
+x = 20
+y = 25 
+z = 30
+
+print((z-y)*x)
+print(x+z/y)
+print(z/x)
+print(z//x)
+
+
+print(x**z)
+print (z%y)
+
+
